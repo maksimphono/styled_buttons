4717e60b4393d1214d8d104d042bfab5205a7aee
Maxun <trukhinmaksimof@mail.ru>
2022-11-24 00:23:15 +0800

    created .scss file with button styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PreviewLayout from "./Components/PreviewLayout.jsx";
+import "./css/App_style.scss";
+
+function App() {
+  return (
+    <PreviewLayout>
+
+    </PreviewLayout>
+  );
+}
+
+export default App;

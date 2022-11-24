57193aa604ddebf93aada80eecc6e93502aca999
Maxun <trukhinmaksimof@mail.ru>
2022-11-24 23:16:39 +0800

    realized simple buttons style using scss, starting to work with component's properties

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,24 @@
 import "../css/button.scss";
+import {useEffect, useRef} from "react";
+import $ from "jquery";
 
-export default function Button ({children, variant}) {
+export default function Button ({children, variant, size, color}) {
+    const buttonRef = useRef();
+    
+    useEffect(() => {
+        //const variant = variant.slice(0, variant.indexOf(' '))
+        const $button = $(buttonRef.current);
 
+        $button.addClass(variant);
+        $button.addClass(size);
+        $button.addClass(color);
+        console.log($button);
+
+        return () => $button.removeClass();
+    });
 
     return (
-        <button className = "lg">
+        <button ref = {buttonRef} className = "md">
             {children}
         </button>
     )

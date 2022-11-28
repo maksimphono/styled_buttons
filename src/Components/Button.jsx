6926be783b653b23feba2b17f21fa9da1e85c461
Maxun <trukhinmaksimof@mail.ru>
2022-11-28 19:35:43 +0800

    created buttons layout

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,24 +2,27 @@ import "../css/button.scss";
 import {useEffect, useRef} from "react";
 import $ from "jquery";
 
-export default function Button ({children, variant, size, color}) {
+export default function Button ({children, variant, size, color, disabled, disableShadow, startIcon, endIcon, className}) {
+    const IconComponent = ({value, hide}) => (<i style={{display: (hide)?"none":"inline"}} className = "material-icons">{value}</i>);
     const buttonRef = useRef();
     
     useEffect(() => {
-        //const variant = variant.slice(0, variant.indexOf(' '))
         const $button = $(buttonRef.current);
 
+        $button.addClass(className);
         $button.addClass(variant);
-        $button.addClass(size);
+        $button.addClass(size || "md");
         $button.addClass(color);
-        console.log($button);
+        $button.addClass(disableShadow && "disable-shadow");
 
         return () => $button.removeClass();
     });
 
     return (
-        <button ref = {buttonRef} className = "md">
+        <button ref = {buttonRef} disabled = {disabled}>
+            <IconComponent value = {startIcon || ""}/>
             {children}
+            <IconComponent value = {endIcon || ""}/>
         </button>
     )
 }
--- src/Components/PreviewLayout.jsx
+++ src/Components/PreviewLayout.jsx
@@ -7,8 +7,119 @@ export default function PreviewLayout (props) {
         <>
             <h1>Buttons</h1>
             <div className = "PreviewLayout">
-                <Button variant = "outline" size = "sm" color = "danger">Default</Button>
+                <div>
+                    <div>
+                        <span>{"<Button />"}</span>
+                        <Button>Default</Button>
+                    </div>
+                    <div>
+                        <span>{":hover, :focus"}</span>
+                        <Button className = "hover">Default</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{"<Button varian='outline' />"}</span>
+                        <Button variant="outline">Default</Button>
+                    </div>
+                    <div>
+                        <span>{":hover, :focus"}</span>
+                        <Button variant='outline' className = "hover">Default</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{"<Button variant='text' />"}</span>
+                        <Button variant = "text">Default</Button>
+                    </div>
+                    <div>
+                        <span>{":hover, :focus"}</span>
+                        <Button variant = "text" className = "hover">Default</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{"<Button disableShadow />"}</span>
+                        <Button disableShadow>Default</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{"<Button disabled />"}</span>
+                        <Button disabled>Default</Button>
+                    </div>
+                    <div>
+                        <span>{"<Button disableShadow />"}</span>
+                        <Button disabled className = "hover">Default</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{"<Button startIcon = 'face' />"}</span>
+                        <Button startIcon = "face">Default</Button>
+                    </div>
+                    <div>
+                        <span>{"<Button endIcon = 'face' />"}</span>
+                        <Button endIcon="face">Default</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{"<Button size = 'sm' />"}</span>
+                        <Button size = "sm">Default</Button>
+                    </div>
+                    <div>
+                        <span>{"<Button size='md' />"}</span>
+                        <Button size='md'>Default</Button>
+                    </div>
+                    <div>
+                        <span>{"<Button size='lg' />"}</span>
+                        <Button size='lg'>Default</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{"<Button color='default' />"}</span>
+                        <Button color='default'>Default</Button>
+                    </div>
+                    <div>
+                        <span>{"<Button color='primary' />"}</span>
+                        <Button color='primary'>Primary</Button>
+                    </div>
+                    <div>
+                        <span>{"<Button color='secondary' />"}</span>
+                        <Button color='secondary'>Secondary</Button>
+                    </div>
+                    <div>
+                        <span>{"<Button color='danger' />"}</span>
+                        <Button color='danger'>Danger</Button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>{":hover, :focus"}</span>
+                        <Button className = "hover" color='default'>Default</Button>
+                    </div>
+                    <div>
+                        <Button className = "hover" color='primary'>Primary</Button>
+                    </div>
+                    <div>
+                        <Button className = "hover" color='secondary'>Secondary</Button>
+                    </div>
+                    <div>
+                        <Button className = "hover" color='danger'>Danger</Button>
+                    </div>
+                </div>
+                <section>
+                    Hey there {"✋"}. My name is Maksim, i'm a beginner front end developer from Russia.<br/>
+                    I made these buttons, using css classes.
+                    Buttons automatically grow to fit content, even if you specify size = 'sm'.<br/>
+                    You can use any Google material icons in 'startIcon' and 'endIcon' and even specify different icons for both of them at the same time.<br/>
+                    Checkout this project on github: <a href = "git@github.com:maksimphono/styled_buttons.git">git@github.com:maksimphono/styled_buttons.git</a> (all last code located in the branch 'feature').
+                    Will be glad, if you rate my work {"😊"}
+                </section>
             </div>
+
         </>
     );
 }
